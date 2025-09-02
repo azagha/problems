7db05202215dc56256cadd29ce2bd7691693e4f0
Anas <anzagha@mbc.net>
2025-09-02 13:53:50 +0300

    6 Done

--- src/main/java/org/example/EncodeAndDecodeStrings.java
+++ src/main/java/org/example/EncodeAndDecodeStrings.java
@@ -1,4 +1,30 @@
 package org.example;
+import java.util.*;
 
 public class EncodeAndDecodeStrings {
+    public String encode(List<String> strs) {
+
+        StringBuilder sol = new StringBuilder();
+        for(String s : strs){
+            sol.append(s.length()).append('@').append(s);
+        }
+        return sol.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i =0;
+        while (i < str.length()) {
+            int j = i;
+            while(str.charAt(j) != '@'){
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i, j));
+            i = j +1;
+            j = i +length;
+            res.add(str.substring(i,j));
+            i = j;
+        }
+        return res;
+    }
 }
